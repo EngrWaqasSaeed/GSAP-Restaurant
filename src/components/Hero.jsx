86ca0b3e458e95b996c1d6a3788acdb5163a4906
EngrWaqasSaeed <waqassaeed633@gmail.com>
2025-08-18 23:02:40 +0500

    Main section completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,20 @@
 import { useGSAP } from '@gsap/react'
-import { SplitText } from 'gsap/all'
 import gsap from 'gsap'
-import React from 'react'
+import { SplitText } from 'gsap/all'
+import { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 gsap.registerPlugin(SplitText)
 
 const Hero = () => {
+  const videoRef = useRef()
+  const videoTimelineRef = useRef()
+  const isMobile = useMediaQuery({ maxWidth: 767 })
   useGSAP(() => {
-    const heroSplit = new SplitText('.title', { type: 'chars, words' })
+    const heroSplit = new SplitText('.title', { type: 'chars , words' })
     const paragraphSplit = new SplitText('.subtitle', { type: 'lines' })
+
+    // Apply text-gradient class once before animating
     heroSplit.chars.forEach(char => char.classList.add('text-gradient'))
     gsap.from(heroSplit.chars, {
       yPercent: 100,
@@ -35,6 +41,25 @@ const Hero = () => {
       })
       .to('.right-leaf', { y: 200 }, 0)
       .to('.left-leaf', { y: -200 }, 0)
+      .to('.arrow', { y: 100 }, 0)
+
+    const startValue = isMobile ? 'top 50%' : 'center 60%'
+    const endValue = isMobile ? '120% top' : 'bottom top'
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'video',
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true
+      }
+    })
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration
+      })
+    }
   }, [])
   return (
     <>
@@ -71,6 +96,15 @@ const Hero = () => {
           </div>
         </div>
       </section>
+      <div className='video absolute inset-0'>
+        <video
+          ref={videoRef}
+          src='../../public/videos/output.mp4'
+          muted
+          playsInline
+          preload='auto'
+        />
+      </div>
     </>
   )
 }
